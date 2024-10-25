a02ea82f330c8dd64adc415f4923272066eab725
axelyurquina12 <axelyurquina2019@gmail.com>
2024-10-25 11:08:30 -0500

    feat: add password

--- conexion.php
+++ conexion.php
@@ -4,7 +4,7 @@ $password = "Sebitas_18";
 
 
 try {
-    $conn = new PDO("sqlsrv:server = tcp:tisnado.database.windows.net,1433; Database = senati.db", "atisnadocha", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:tisnado.database.windows.net,1433; Database = senati.db", "atisnadocha", "Sebitas_18");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -13,7 +13,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "atisnadocha", "pwd" => "{your_password_here}", "Database" => "senati.db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "atisnadocha", "pwd" => "Sebitas_18", "Database" => "senati.db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:tisnado.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
